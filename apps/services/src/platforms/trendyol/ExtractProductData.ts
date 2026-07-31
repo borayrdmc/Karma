3e4ec0fdcba93b7f468a5a981fe1c6aed09a4543
Bora <bborayrdmc@gmail.com>
2026-07-31 13:30:30 +0300

    feat:add JSON extractor

--- apps/services/src/platforms/trendyol/ExtractProductData.ts
+++ apps/services/src/platforms/trendyol/ExtractProductData.ts
@@ -1,7 +1,9 @@
-function extractProductData(html:string){
+import { TrendyolResponseProductData } from "./TrendyolTypes";
+
+export function extractProductData(trendyolRawHtml:string){
 
     const sharedPropsText = 'window["__envoy__SHARED_PROPS"]=';
-    const startIndexOfPropsText = html.indexOf(sharedPropsText);
+    const startIndexOfPropsText = trendyolRawHtml.indexOf(sharedPropsText);
 
     if(startIndexOfPropsText===-1){
         throw new Error("SHARED PROPS not found. Check HTML for changes.");
@@ -9,14 +11,14 @@ function extractProductData(html:string){
 
     const startIndexOfJson = startIndexOfPropsText+sharedPropsText.length;
 
-    const lastIndexOfJson = html.indexOf('</script>',startIndexOfJson);
+    const lastIndexOfJson = trendyolRawHtml.indexOf('</script>',startIndexOfJson);
 
     if(lastIndexOfJson===-1){
         throw new Error("</script> tag not found. Check HTML for changes.")
     }
 
-    const rawStringJSON = html.slice(startIndexOfJson,lastIndexOfJson).trim();
+    const rawStringJSON = trendyolRawHtml.slice(startIndexOfJson,lastIndexOfJson).trim();
     const productDataJSON = JSON.parse(rawStringJSON);
 
-    return productDataJSON;
+    return productDataJSON as TrendyolResponseProductData;
 }
