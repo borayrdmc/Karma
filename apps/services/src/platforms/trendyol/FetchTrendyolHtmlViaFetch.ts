a0723890100b355396b230476002f8d8f8961b91
Bora <bborayrdmc@gmail.com>
2026-07-31 19:12:52 +0300

    feat: Add stealth configuration to fetch

--- apps/services/src/platforms/trendyol/FetchTrendyolHtmlViaFetch.ts
+++ apps/services/src/platforms/trendyol/FetchTrendyolHtmlViaFetch.ts
@@ -13,12 +13,11 @@ export async function fetchTrendyolHtmlViaFetch(productUrl: string): Promise<str
         const response = await fetch(productUrl,{
             signal: AbortSignal.timeout(10000),
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
                 'Accept-Language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-                'Accept-Encoding': 'gzip, deflate, br, zstd',
-                'Referer': 'https://www.google.com/',
-                'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+                'Referer': 'https://www.google.com/', //Make it look like we came from google like a "human :p"
+                'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124", "Google Chrome";v="124"', //Dunno anything about headers beyond this point
                 'sec-ch-ua-mobile': '?0',
                 'sec-ch-ua-platform': '"Windows"',
                 'sec-fetch-dest': 'document',
@@ -26,7 +25,6 @@ export async function fetchTrendyolHtmlViaFetch(productUrl: string): Promise<str
                 'sec-fetch-site': 'cross-site',
                 'sec-fetch-user': '?1',
                 'Upgrade-Insecure-Requests': '1',
-                'Priority': 'u=0, i',
             }
         });
 
@@ -39,6 +37,11 @@ export async function fetchTrendyolHtmlViaFetch(productUrl: string): Promise<str
         }
 
         const trendyolRawHtml = await response.text();
+
+        if(!trendyolRawHtml.includes('window["__envoy__SHARED_PROPS"]=')){
+            throw new Error("HTML fetched successfully but product data is missing. Captcha, WAF or invalid response."); //Sent to a cloudflare security check or captcha check
+        }
+
         return trendyolRawHtml;
     }
     catch(error){
