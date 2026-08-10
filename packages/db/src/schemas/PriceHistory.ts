91a2e72094033164be3979f1c0b87065239640a7
Bora <bborayrdmc@gmail.com>
2026-08-10 17:55:59 +0300

    feat: add price history table

---
+++ packages/db/src/schemas/PriceHistory.ts
@@ -0,0 +1,14 @@
+import { index, numeric, pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
+import { products } from "./Products";
+
+export const priceHistory = pgTable("price_history",{
+
+    id: uuid('id').primaryKey().defaultRandom(),
+    productId : uuid('product_id').notNull().references(()=>products.productId,{onDelete: "cascade"}),
+    price: numeric("price", { precision: 10, scale: 2 }).notNull(),
+    checkedAt: timestamp("checked_at").defaultNow().notNull(),
+    },
+    (table)=>[
+        index("time_and_id_unique").on(table.productId,table.checkedAt)
+    ]   
+)
