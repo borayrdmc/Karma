61d735c088af43b8e5a47b76c84aa195d39d18b1
Bora <bborayrdmc@gmail.com>
2026-08-10 17:54:17 +0300

    feat: add products table

---
+++ packages/db/src/schemas/Products.ts
@@ -0,0 +1,20 @@
+import { pgEnum, pgTable, text, unique, uuid } from "drizzle-orm/pg-core";
+
+export const platformEnum = pgEnum('platform_type',[
+  'trendyol',
+  'hepsiburada'
+]);
+
+export const products = pgTable("products",{
+
+    productId: uuid("product_id").primaryKey().defaultRandom(),
+    productUrl: text("product_url").notNull(),
+	productName: text("product_name"),
+    productPlatform: platformEnum("product_platform").notNull(),
+    productCode: text("product_code").notNull(),
+	},
+	//Schema Configs
+	(table)=>[
+		unique('product_code_unique').on(table.productPlatform,table.productCode),
+	]
+)
