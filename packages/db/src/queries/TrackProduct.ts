bb5131a4e27fa876903dbd97954d3884ccbd6dbc
Bora <bborayrdmc@gmail.com>
2026-08-11 23:34:03 +0300

    refactor: shared type refactor

--- packages/db/src/queries/TrackProduct.ts
+++ packages/db/src/queries/TrackProduct.ts
@@ -1,13 +1,8 @@
+import { TrackedProductDataType } from "@repo/types";
 import { db } from "../client";
 import { trackedProducts } from "../schemas";
 
-interface TrackedProductData{
-
-    userId:string;
-    productId:string;
-}
-
-export async function trackProduct({userId,productId}:TrackedProductData){
+export async function trackProduct({userId,productId}:TrackedProductDataType){
     
     const trackedProduct=await db.insert(trackedProducts).values({userId,productId}).returning();
 
