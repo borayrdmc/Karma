03eed65b31f43b8844d238a0b52ae0a20768e74e
Bora <bborayrdmc@gmail.com>
2026-08-15 22:05:06 +0300

    feat: add detailed errors

--- apps/services/src/platforms/trendyol/ExtractProductData.ts
+++ apps/services/src/platforms/trendyol/ExtractProductData.ts
@@ -1,24 +1,37 @@
+import { ServiceError } from "@repo/errors";
 import { TrendyolResponseProductData } from "./TrendyolTypes";
 
 export function extractProductData(trendyolRawHtml:string){
 
-    const sharedPropsText = 'window["__envoy__SHARED_PROPS"]=';
-    const startIndexOfPropsText = trendyolRawHtml.indexOf(sharedPropsText);
+    try{
+        const sharedPropsText = 'window["__envoy__SHARED_PROPS"]=';
+        const startIndexOfPropsText = trendyolRawHtml.indexOf(sharedPropsText);
 
-    if(startIndexOfPropsText===-1){
-        throw new Error("SHARED PROPS not found. Check HTML for changes.");
+        if(startIndexOfPropsText===-1){
+            throw new ServiceError("Source HTML code changed.",502);
+        }
+
+        const startIndexOfJson = startIndexOfPropsText+sharedPropsText.length;
+
+        const lastIndexOfJson = trendyolRawHtml.indexOf('</script>',startIndexOfJson);
+
+        if(lastIndexOfJson===-1){
+            throw new ServiceError("Source HTML code changed.",502);
+        }
+
+        const rawStringJSON = trendyolRawHtml.slice(startIndexOfJson,lastIndexOfJson).trim();
+        const productDataJSON = JSON.parse(rawStringJSON);
+
+        return productDataJSON as TrendyolResponseProductData;
+    }   
+    catch(parsingError){
+
+        if(parsingError instanceof ServiceError){
+            throw parsingError
+        }
+        if(parsingError instanceof SyntaxError){
+            throw new ServiceError("JSON parse failed",502);
+        }
+        throw new ServiceError("Unknown error",500,{cause:parsingError});
     }
-
-    const startIndexOfJson = startIndexOfPropsText+sharedPropsText.length;
-
-    const lastIndexOfJson = trendyolRawHtml.indexOf('</script>',startIndexOfJson);
-
-    if(lastIndexOfJson===-1){
-        throw new Error("</script> tag not found. Check HTML for changes.")
-    }
-
-    const rawStringJSON = trendyolRawHtml.slice(startIndexOfJson,lastIndexOfJson).trim();
-    const productDataJSON = JSON.parse(rawStringJSON);
-
-    return productDataJSON as TrendyolResponseProductData;
 }
