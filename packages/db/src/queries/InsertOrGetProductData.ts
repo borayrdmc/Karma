5be99ccf55709198de42bed8476191241d4e6088
Bora <bborayrdmc@gmail.com>
2026-08-15 22:08:20 +0300

    feat: add detailed errors

--- packages/db/src/queries/InsertOrGetProductData.ts
+++ packages/db/src/queries/InsertOrGetProductData.ts
@@ -1,4 +1,5 @@
 import { and, eq } from "drizzle-orm";
+import { ServiceError } from "@repo/errors";
 import { db } from "../client";
 import { products } from "../schemas";
 import { ProductDataType } from "@repo/types";
@@ -12,7 +13,14 @@ export async function insertOrGetProductData({productUrl,productPlatform,product
         return existingProducts[0]
     }
 
-    const insertedProduct=await db.insert(products).values({productUrl,productPlatform,productCode,productName}).returning()
+    const insertedProduct=await db.insert(products)
+        .values({productUrl,productPlatform,productCode,productName})
+        .onConflictDoNothing()
+        .returning();
+
+    if(insertedProduct.length===0){
+        throw new ServiceError("Something went wrong while adding the product. Please try again",409);
+    }
 
     return insertedProduct[0];
 }
