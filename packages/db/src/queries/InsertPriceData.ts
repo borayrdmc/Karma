910387698a14bfd0d487797870e86c5b183569ec
Bora <bborayrdmc@gmail.com>
2026-08-19 12:37:57 +0300

    feat: removed files

--- packages/db/src/queries/InsertPriceData.ts
+++
@@ -1,9 +0,0 @@
-import { PriceDataType } from "@repo/types";
-import { db } from "../client";
-import { priceHistory } from "../schemas";
-
-export async function insertPriceData({price,productId}:PriceDataType){
-
-    const priceData=await db.insert(priceHistory).values({productId,price}).returning();
-    return priceData[0];
-}
