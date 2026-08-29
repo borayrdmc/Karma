0e5cef8699e0a703f42ca289a42a9e545d549956
Bora <bborayrdmc@gmail.com>
2026-08-29 20:28:58 +0300

    feat: session table

---
+++ packages/db/src/schemas/auth/session.ts
@@ -0,0 +1,13 @@
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { user } from "./user";
+
+export const session=pgTable("session",{
+
+    id:text("session_id").primaryKey().notNull(),
+    token:text("session_token").notNull().unique(),
+    userId:text("session_user_id").notNull().references(()=>user.id, {onDelete:"cascade"}),
+    createdAt:timestamp("session_created_at").notNull().defaultNow(),
+    expiresAt:timestamp("session_expires_at").notNull(),
+    updatedAt:timestamp("session_updated_at").notNull().defaultNow()
+    }
+)
